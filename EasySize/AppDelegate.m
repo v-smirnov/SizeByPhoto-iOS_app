af7cc00c9329c25e3b8c30afd47e19dea96e8c2c
Vladimir <capoeiru@gmail.com>
2013-01-04 17:15:46 +0400

    040113_1715

--- EasySize/AppDelegate.m
+++ EasySize/AppDelegate.m
@@ -81,7 +81,6 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-    // if image picker controller is active don't pop to root controller
     if (!imagePickerControllerIsActive){
         [self.navigationController popToRootViewControllerAnimated:NO];
     }
